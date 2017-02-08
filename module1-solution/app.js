d0dd13ecd6b3e97c6fd34224f5cbe0415a82c1a3
Dmitry Zhdanov <zhdanovd@gmail.com>
2017-02-08 15:09:57 +0300

    colorise
    Change to style

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,7 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope','$filter'];
 function LunchCheckController($scope, $filter) {
   var lunchInputList = "";
+  $scope.customStyle = {};
   $scope.saveLunchList = function () {
     lunchInputList = $scope.lunchList.toString();
   };
@@ -15,15 +16,15 @@ function LunchCheckController($scope, $filter) {
 
     if (lunchInputList.length == 0) {
       $scope.lunchCheckResult = "Please enter data first";
-      $scope.color = "red"
+      $scope.customStyle.style = {"color":"red"};
     } else if (lunchSplitedList.length > 3) {
       console.log('More than 3');
       $scope.lunchCheckResult = "Too much!";
-      $scope.color = "green"
+      $scope.customStyle.style = {"color":"green"};
     } else {
       console.log('Less or equal 3');
       $scope.lunchCheckResult = "Enjoy!";
-      $scope.color = "green"
+      $scope.customStyle.style = {"color":"green"};
     };
   };
 };
